5f689a80696a557b18fa8b67de901ac6aee1ea92
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2026-01-14 13:18:52 +0300

    Add project files.

---
+++ Contacts.Core/Models/Contact.cs
@@ -0,0 +1,46 @@
+﻿namespace Contacts.Core.Models
+{
+    public class Contact
+    {
+        const int MAX_NAME_LENGHT = 100; 
+        const int MAX_JOB_TITLE_LENGHT = 100; 
+        private Contact(Guid id, string name, string mobilePhone, string jobTitle, DateOnly birthDate)
+        {
+            Id = id;
+            Name = name;
+            MobilePhone = mobilePhone;
+            JobTitle = jobTitle;
+            BirthDate = birthDate;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string MobilePhone { get; set; }
+        public string JobTitle { get; set; }
+        public DateOnly BirthDate { get; set; }
+
+        public static (Contact contact, string Error) Create(Guid id, string name, string mobilePhone, string jobTitle, DateOnly birthDate)
+        {
+            var error = string.Empty;
+            if (string.IsNullOrEmpty(name) || name.Length > MAX_NAME_LENGHT)
+            {
+                error = $"Name cannot be empty or longer than {MAX_NAME_LENGHT} symbols";
+            }
+            if (string.IsNullOrEmpty(jobTitle) || jobTitle.Length > MAX_JOB_TITLE_LENGHT)
+            {
+                error = $"Job title cannot be empty or longer than {MAX_JOB_TITLE_LENGHT} symbols";
+            }
+            if (string.IsNullOrEmpty(mobilePhone))
+            {
+                error = "Mobile phone cannot be empty";
+            }
+            if (birthDate > DateOnly.FromDateTime(DateTime.Today))
+            {
+                error = "Date cannot be in the future";
+            }
+            var contact = new Contact(id, name, mobilePhone, jobTitle, birthDate);
+
+            return (contact, error);
+        }
+    }
+}
