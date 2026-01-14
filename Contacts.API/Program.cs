79d35136b5459d0cacee8f3336f08680e299ed45
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2026-01-14 16:00:39 +0300

    add app host

--- Contacts.API/Program.cs
+++ Contacts.API/Program.cs
@@ -1,17 +1,43 @@
+using Contacts.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.AddServiceDefaults();
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContactsDbContext>(
+    options =>
+    {
+        options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(ContactsDbContext)));
+    });
+
+//builder.Services.AddScoped<IContactsServices, ContactsService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapDefaultEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors(x =>
+{
+    x.WithHeaders().AllowAnyHeader();
+    x.WithOrigins("http://localhost:5173");
+    x.WithMethods().AllowAnyMethod();
+});
 
 app.Run();
